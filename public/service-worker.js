45b93bc3d5ec3514a2d3a3c21881d2b754190f78
Matthew Sievers <mattersievers@gmail.com>
2021-11-05 22:24:32 -0500

    website cache storage works

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,8 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "index.html",
-    "/css/style.css",
+    "/index.html",
+    "/css/styles.css",
     "/js/idb.js",
     "/js/index.js"
 ];
